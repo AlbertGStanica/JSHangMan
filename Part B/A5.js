a2784c22cceddb454becadd917ce07e215b62281
Nevarre Sokolowski <nsok615@gmail.com>
2025-03-23 10:45:17 -0300

    chipping away

--- Part B/A5.js
+++ Part B/A5.js
@@ -1,12 +1,18 @@
 let wordObj;
 
 window.onload = function () {
-    loadData();
-    let letterContain = document.querySelector(".letterContain");
-    letterContain.addEventListener("click", checkLetter);
+    LoadData();
+    let letterContain = document.querySelector("#letterContain");
+    letterContain.addEventListener("click", CheckLetter);
+
+    let buttonNewGame = document.querySelector("#startNew");
+    buttonNewGame.addEventListener("click", CallDropDown);
+
+    let buttonStart = document.querySelector("#begin");
+    buttonStart.addEventListener("click", StartGame);
 }
 
-function loadData() {
+function LoadData() {
     let url = "vocabularies.json"; // file we want to read
     let xhr = new XMLHttpRequest(); //step 1
     xhr.onreadystatechange = function () {
@@ -14,14 +20,28 @@ function loadData() {
         // code 200 means the server succeeded in retrieving the resource
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
             // console.log(xhr.response);
-            wordObj = JSON.parse(xhr.responseText); //contents of json file 
+            wordObj = JSON.parse(xhr.responseText); //contents of json file
+            CallDropDown();
         }
     };
     xhr.open("GET", url, true); // must use “GET” method :step3
     xhr.send(); // this actually sends the request to the server :step 4
 }
 
-function checkLetter(evt) {
+function CallDropDown() {
+    let dropDown = document.querySelector(".dropDownContain");
+
+    if (dropDown.classList.contains("show")) {
+        dropDown.classList.remove("show");
+        dropDown.classList.add("hide");
+    }
+    else {
+        dropDown.classList.remove("hide");
+        dropDown.classList.add("show");
+    }
+}
+
+function CheckLetter(evt) {
     let click = evt.target;
 
     if (click.classList.contains("letter")) {
@@ -29,7 +49,35 @@ function checkLetter(evt) {
     }
 }
 
-function gameOnLoad() {
+function GenerateWord() {
+    let radSelect = document.querySelector('input[type=radio]:checked');
+    let categorySelect = radSelect.value;
+    let categoryArray = wordObj.vocabularies;
+    let randomWord = "";
 
-    newGame();
+    for (i = 0; i < categoryArray.length; i++) {
+        if (categoryArray[i].categoryName === categorySelect) {
+            let wordList = categoryArray[i].words;
+            randomWord = wordList[Math.round((Math.random() * wordList.length))];
+
+            console.log(randomWord);
+        }
+    }
+
+    return randomWord;
+}
+
+function StartGame() {
+    alert("New game started! New Game Button disabled. Good luck.");
+    CallDropDown();
+    let buttonNewGame = document.querySelector("#startNew");
+    buttonNewGame.removeEventListener("click", CallDropDown);
+
+    let mysteryWord = GenerateWord();
+
+    newGame(mysteryWord);
+
+    console.log(mysteryWord);
+
+
 }
